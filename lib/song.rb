fc193eef458dd44e3a89a2a49df9d25b39d9cfbb
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-21 15:29:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,28 +1,35 @@
 class Song
-  attr_accessor :name,:song, :artist
+  attr_accessor :name, :artist
+
   @@all = []
+
   def initialize(name)
     @name = name
   end
-  def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
+
+  def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song.save
+  end
+
+  def self.find_by_artist(artist)
+    Song.all.select do | song |
+      song.artist == artist
     end
   end
 
+  def self.all
+    @@all
+  end
 
-  def self.new_by_filename(filename)
-      artist, song = filename.split(" - ")
-      new_song = self.new(song)
-      new_song.artist_name = artist
-      new_song.save
-    end
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+  end
 
-    def save
-        @@all << self
-        self
-      end
-
+  def save
+    @@all << self
+    self
+  end
 end
