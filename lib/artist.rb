7108c6c418a57fb3226db509e80b5632e05a1dd2
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-21 13:29:17 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,7 @@ class Artist
 
   def self.find_or_create_by_name(name)
     if (self.name.nil?)
-      self = self.new(name)
+      artist = self.new(name)
     else
       self.name = name
     end
