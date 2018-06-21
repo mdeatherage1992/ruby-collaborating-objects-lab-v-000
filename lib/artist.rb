8656f443b5b980d65c25194be9f8b926554af5e7
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-21 12:15:03 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,14 @@
 class Artist
-  attr_accessor :name
-
+  attr_accessor :name, :songs
+  @@all = []
   def initialize(name)
     @name = name
+    @songs = []
   end
+
+  def songs
+    @songs
+  end
+
 
 end
