676e865f49fc5ef15ddf1ddae3f4b4b76c63edc9
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-21 15:27:00 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,10 @@
 require "pry"
 class Artist
-  attr_accessor :name
   @@all = []
+  attr_accessor :name
+
   def initialize(name)
     @name = name
-    @songs = []
-  end
-
-  def add_song(song)
-    song.name = self
-    @songs << song
-  end
-
-  def save
-    @@all << self
   end
 
   def self.all
@@ -21,25 +12,27 @@ class Artist
   end
 
   def songs
-     Song.find_by_artist(self)
-   end
-   
+    Song.find_by_artist(self)
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+
+  def self.create(name)
+    self.new(name).tap {|artist| artist.save}
+  end
+
+  def save
+    @@all << self
+  end
+
   def print_songs
     songs.each {|song| puts song.name}
   end
-
-
-  def self.find_or_create_by_name(name)
-    existing_artist = ''
-    @@all.each do |artist|
-      if artist.name == name
-        existing_artist = artist
-      end
-    end
-    if existing_artist == ''
-      existing_artist = self.new(name)
-    end
-    existing_artist
-  end
-
 end
