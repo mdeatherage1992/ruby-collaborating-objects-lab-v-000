53a76c26096723429ec348b90f4ace5fa8f4d1bf
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-21 12:29:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,4 +11,14 @@ class Song
       self.artist.name = name
     end
   end
+
+  def self.new_by_filename(filename)
+    song = self.new
+    song.title = filename.split(" - ")[1]
+    song
+  end
+
+
+
+
 end
