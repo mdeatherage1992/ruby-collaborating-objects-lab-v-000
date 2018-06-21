53cd6c1beb1e466411ba6c6be87021fd5db74fe2
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-21 12:23:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,3 +4,11 @@ class Song
   def initialize(name)
     @name = name
   end
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+end
