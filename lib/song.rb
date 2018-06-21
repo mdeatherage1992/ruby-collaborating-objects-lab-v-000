425fd4561df06fe8d79d59b9178b9784a861d25e
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-21 14:43:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,8 +13,7 @@ class Song
   end
 
   def self.new_by_filename(file_name)
-    song = self.new
-     song.title = file_name.split(" - ")[1]
+    song = self.new(file_name.split(" - ")[1])
      song
   end
 
